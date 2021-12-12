e12972024a6e4813467791bab3635085dfab3522
panchunbo <pcb1712379335@gmail.com>
2021-12-12 17:24:45 +0800

    add the udp socket class and modify CMake txt

--- include/type.h
+++ include/type.h
@@ -1,10 +1,17 @@
 #ifndef _TYPE_H_
 #define _TYPE_H_
 
-#define FALSE 0
-#define TRUE 1
+#define FALSE   0
+#define TRUE    1
+
+#define PROCESS_FAIL    0
+#define PROCESS_SUCCESS 1
+
 typedef unsigned int uint32_t;
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned long uint64_t;
+
+#define LINUX_ERROR_PRINT() printf("%s\n",strerror(errno));
+
 #endif
--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include "type.h"
+#include "net.h"
+#include <unistd.h>
 
 int main()
 {
-    std::cout<<"hello world"<<std::endl;
+    net::udpSocket udpClass;
+    std::string bindIp("127.0.0.100:2152");
+    udpClass.bindSocket(bindIp);
+    while(1)
+    {
+        sleep(1);
+    }
 }
